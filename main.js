32aeecb5f8f0c6a34676fc38034b926678d1849a
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-04-14 07:50:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,32 @@ camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
 
-function takeSnapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="captured_img" src="'+ data_uri + '"/>';
+function takeSnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="' + data_uri + '"/>';
     });
 }
 
 console.log('ML5 version: ', ml5.version);
 
+function modelLoaded() {
+    console.log("model loaded");
+}
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/a7CJ9EQT-/model.json', modelLoaded());
 
-function modelLoaded(){
-    console.log("model loaded");
+
+
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
