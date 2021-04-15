485d52b6be41a366ab22443d911bf8aa18cada2b
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-04-15 14:14:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,25 +14,26 @@ function takeSnapshot() {
         document.getElementById("result").innerHTML = '<img id="captured_img" src="' + data_uri + '"/>';
     });
 }
-
 console.log('ML5 version: ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7eUiUtY4V/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log("model loaded");
 }
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/a7CJ9EQT-/model.json', modelLoaded());
 
-
-
-function check() {
+function Check() {
+    console.log("Inside Check Function");
     img = document.getElementById("captured_img");
-    classifier.classify(img , gotResult);
+    classifier.classify(img, gotResult);
 }
 
 function gotResult(error, results) {
+    // Display error in the console 
     if (error) {
         console.error(error);
     } else {
+        // The results are in an array ordered by confidence. 
         console.log(results);
         document.getElementById("object").innerHTML = results[0].label;
         document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
